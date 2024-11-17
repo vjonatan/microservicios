3d434bba59622c2017f1321831f311acc1ece5de
Jonatan <jonatan.e.vera@gmail.com>
2024-11-17 17:49:44 -0300

    Version inicial del ms api-auth-service

---
+++ api-auth-service/src/main/java/com/auth/jwt/ApiAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthServiceApplication.class, args);
+	}
+
+}
